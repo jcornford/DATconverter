9c64b86c16a0bf628d044d4ff64777378dc58dc9
JonathanCornford <jonathan.cornford@gmail.com>
2015-07-12 12:42:45 +0100

    Update v01.py

--- v01.py
+++ v01.py
@@ -1,23 +1,35 @@
-import struct as st
-#2015_01_14_s2c1_i75.DAT
-#'/2015_01_14_isteps.DAT'
+import numpy as np
+import struct
+
+path = '/Users/Jonathan/Documents/PhD /Data/2015'
+foldername = '/2015_01/'
+folderpath = path+foldername
+filename = '2015_01_14_isteps.DAT'
+filepath = fpath+'/2015_01_14_isteps.DAT'
+#savepath = folderpath+'converted/'+filename[:-4]
+savepath = folderpath+filename[:-4]
+
 bytes = []
 data = []
-first = True
 
-byte_indexes = [611, 320640,640669, 960698 ]
-sweep_number = 4
+first = True
+channel_number = 4
+fs = 20000
+acq_len = 1 # in seconds 
+sweep_number = 30
+
+bytes_per_sweep = fs*4*channel_number
+
+byte_indexes = [(bytes_per_sweep*i) +611+(29*i) for i in range(sweep_number)]
 for sweep_index in range(sweep_number):
-    print sweep_index, 'sweep_index'
-    with open(fpath+'/2015_01_14_isteps.DAT', "rb") as f:
+    with open(filepath, "rb") as f:
         wastebyte = f.read(byte_indexes[sweep_index])
         data = []
-
         for i in range(80000): # channel*sampling
             byte = f.read(4)
             bytes.append(byte)
             try:
-                x = st.unpack('>f',byte)
+                x = struct.unpack('>f',byte)
                 data.append(x)
             except:
                 print 'Error', Exception
@@ -28,14 +40,18 @@ for sweep_index in range(sweep_number):
             vals = np.ravel(np.array([data]))
             dArray = np.vstack([dArray,vals])
 
-dArray = dArray.transpose()    
-print dArray.shape
+dArray = dArray.transpose()
+# rows are time, columns sweeps, depth is channels
+if channel_number == 4:
+    temp0 = dArray[0::4]
+    temp1 = dArray[1::4]
+    temp2 = dArray[2::4]
+    temp3 = dArray[3::4]
+    dArray = np.dstack([temp0,temp1,temp2,temp3])
 
+if channel_number ==2:
+    temp0 = dArray[0::2]
+    temp1 = dArray[1::2]
+    dArray = np.dstack([temp0,temp1])
 
-plt.figure(figsize = (12,6))
-plt.subplot(1,2,1)
-plt.plot(dArray[0::4,:])
-plt.axis('off')
-plt.subplot(1,2,2)
-plt.plot(dArray[2::4], 'k')
-plt.axis('off')
+np.save(savepath,dArray)
